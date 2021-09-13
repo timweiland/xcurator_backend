c924f35d4f07bf6c856fbf8ad66ea842d9dd11da
Tim Weiland <tim@weiland-lahnstein.de>
2021-09-13 16:47:22 +0200

    Fix auth token permissions
    Previously it was not possible to delete users that had outstanding
    tokens because the admin did not have permission to delete those tokens.
    This is now fixed.

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from rest_framework_simplejwt import token_blacklist
+from rest_framework_simplejwt.token_blacklist.admin import OutstandingTokenAdmin
 from users.models import User
+
+
+class NewOutstandingTokenAdmin(OutstandingTokenAdmin):
+    def has_delete_permission(self, *args, **kwargs):
+        return True
+
+
+admin.site.unregister(token_blacklist.models.OutstandingToken)
+admin.site.register(token_blacklist.models.OutstandingToken, NewOutstandingTokenAdmin)
 
 
 class UserAdmin(admin.ModelAdmin):
